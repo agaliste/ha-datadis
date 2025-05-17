e7469b3aea657c2c4c6b6594361e86eaa96ccda3
Adrian Galisteo <28538827+agaliste@users.noreply.github.com>
2025-05-17 19:33:13 +0200

    Update domain name to 'ha-datadis' in constants and manifest files, and add 'test_mode' configuration option.

--- custom_components/ha-datadis/const.py
+++ custom_components/ha-datadis/const.py
@@ -1,7 +1,8 @@
 """Constants for the Datadis integration."""
 
-DOMAIN = "datadis"
+DOMAIN = "ha-datadis"
 
 CONF_CUPS = "cups"
 CONF_DISTRIBUTOR_CODE = "distributor_code"
 CONF_POINT_TYPE = "point_type"
+CONF_TEST_MODE = "test_mode"
